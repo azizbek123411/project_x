ae4d5f261fc575a78f5583c63a033127f554deb9
hp <sultonovazizbek73@gmail.com>
2025-05-16 00:33:13 +0500

    post box

--- lib/pages/navbar_pages/home_page.dart
+++ lib/pages/navbar_pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project_x/widgets/my_input_alert_box.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +9,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+final controller=TextEditingController();
+
+
+  void _openMessageBox() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return 
+             MyInputAlertBox(
+              controller: controller,
+              hintTitle: 'New Post',
+              onTap: (){},
+              onPressedText: 'Post',
+            );
+        
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _openMessageBox,
+        child: Icon(Icons.add),
       ),
     );
   }
--- lib/service/database/database_provider.dart
+++ lib/service/database/database_provider.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:project_x/models/post.dart';
 import 'package:project_x/models/user.dart';
 import 'package:project_x/service/database/database_service.dart';
 
-class DatabaseProvider extends ChangeNotifier{
+class DatabaseProvider extends ChangeNotifier {
+  final _db = DatabaseService();
 
-    final _db=DatabaseService();
+  Future<UserProfile?> userProfile(String uid) => _db.getUserFromFirebase(uid);
 
+  Future<void> updateUserBio(String bio) => _db.updateUserBioInFirebase(bio);
 
-    Future<UserProfile?> userProfile(String uid)=> _db.getUserFromFirebase(uid);
+  List<Post> allPosts = [];
+  List<Post> get getAllPosts => allPosts;
 
-
-
-    Future<void> updateUserBio(String bio)=>_db.updateUserBioInFirebase(bio);
+  Future<void> postMessage(String message) async {
+    await _db.postMessageInFirebase(message);
+  }
 }
