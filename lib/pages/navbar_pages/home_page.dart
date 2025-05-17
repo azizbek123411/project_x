0a9445e1f55e8e3fab32ed83d604ad5ee8ab0ebc
hp <sultonovazizbek73@gmail.com>
2025-05-17 14:21:59 +0500

    display posts

--- lib/pages/navbar_pages/home_page.dart
+++ lib/pages/navbar_pages/home_page.dart
@@ -3,6 +3,8 @@ import 'package:project_x/service/database/database_provider.dart';
 import 'package:project_x/widgets/my_input_alert_box.dart';
 import 'package:provider/provider.dart';
 
+import '../../models/post.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -14,6 +16,8 @@ class _HomePageState extends State<HomePage> {
   final controller = TextEditingController();
   late final databaseProvider =
       Provider.of<DatabaseProvider>(context, listen: false);
+
+  late final listeningProvider = Provider.of<DatabaseProvider>(context);
 
   void _openMessageBox() {
     showDialog(
@@ -35,15 +39,53 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  void initState() {
+    super.initState();
+
+    loadAllPosts();
+  }
+
+  Future<void> loadAllPosts() async {
+    await databaseProvider.loadAllPosts();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
+      body: buildPostList(listeningProvider.getAllPosts),
       floatingActionButton: FloatingActionButton(
         onPressed: _openMessageBox,
         child: Icon(Icons.add),
       ),
     );
+  }
+
+  Widget buildPostList(List<Post> posts) {
+    return posts.isEmpty
+        ? Center(
+            child: Text(
+              'No posts available',
+              style: TextStyle(fontSize: 18),
+            ),
+          )
+        : ListView.builder(
+            itemCount: posts.length,
+            itemBuilder: (context, index) {
+              final post = posts[index];
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8,right: 8,bottom: 4),
+                    child: Text(post.message,style: TextStyle(fontSize: 18),),
+                  ),
+                  Divider(),
+                ],
+              );
+            });
   }
 }
